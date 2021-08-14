f96a13f4f9a44118a2a549508bb885664fc9ce80
ayush-rawat-7 <rwat905@gmail.com>
2021-08-14 13:45:34 +0530

    END

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import './App.css';
+import React from 'react';
 import { Sidebar } from './Sidebar';
+import Pusher from 'pusher-js'
 import { Chat } from './Chat';
+import axios from "./axios"
 function App() {
+  const [messages, setMessages] = React.useState([])
+
+  React.useEffect(() => {
+    axios.get("/messages/sync")
+      .then(response => {
+        setMessages(response.data);
+      })
+  }, [])
+
+  React.useEffect(() => {
+    const pusher = new Pusher('ac776a713111e6b7b924', {
+      cluster: 'mt1'
+    });
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', (newMessage) => {
+      setMessages([...messages, newMessage])
+    });
+
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    }
+  }, [messages]);
+
+  console.log(messages);
+
   return (
     <div className="app">
       <div className="app_body">
         <Sidebar />
-        <Chat />
+        <Chat messages={messages} />
       </div>
     </div>
   );
--- src/Chat.js
+++ src/Chat.js
@@ -2,10 +2,24 @@ import React from 'react'
 import "./chat.css";
 import { Avatar, IconButton } from "@material-ui/core";
 import { AttachFile, MoreVert } from "@material-ui/icons";
+import axios from './axios'
 import SearchIcon from '@material-ui/icons/Search';
 import MicOutlinedIcon from '@material-ui/icons/MicOutlined';
 import EmojiEmotionsOutlinedIcon from '@material-ui/icons/EmojiEmotionsOutlined';
-export const Chat = () => {
+export const Chat = ({ messages }) => {
+    const [input, setInput] = React.useState('');
+
+    const sendMessage = async (e) => {
+        e.preventDefault();
+        await axios.post('/messages/new', {
+            message: input,
+            name: "Demo",
+            timeStamp: "Just Now",
+            recieved: false,
+        })
+        setInput('')
+    }
+
     return (
         <div className="chat">
             <div className="chat_header">
@@ -28,35 +42,23 @@ export const Chat = () => {
             </div>
 
             <div className="chat_body">
-                <p className="chat_message">
-                    <span className="chat_name">Ayush</span>
-                    This is a message
-                    <span className="chat_timeStamp">
-                        {new Date().toUTCString()}
-                    </span>
-                </p>
-                <p className="chat_reciever chat_message">
-                    <span className="chat_name">Ayush</span>
-                    This is a message
-                    <span className="chat_timeStamp">
-                        {new Date().toUTCString()}
-                    </span>
-                </p>
-                <p className="chat_message">
-                    <span className="chat_name">Ayush</span>
-                    This is a message
-                    <span className="chat_timeStamp">
-                        {new Date().toUTCString()}
-                    </span>
-                </p>
+                {messages.map((message) => {
+                    return <p className={`chat_message ${message.recieved || "chat_reciever"} `}>
+                        <span className="chat_name">{message.name}</span>
+                        {message.message}
+                        <span className="chat_timeStamp">
+                            {message.timeStamp}
+                        </span>
+                    </p>
+                })}
             </div>
             <div className="chat_footer">
                 <EmojiEmotionsOutlinedIcon />
                 <form>
-                    <input type="text" placeholder="Type a message" />
-                    <button type="submit">Send a message</button>
+                    <input type="text" value={input} onChange={(e) => setInput(e.target.value)} placeholder="Type a message" />
+                    <button onClick={sendMessage} type="submit">Send a message</button>
                 </form>
-                <MicOutlinedIcon/>
+                <MicOutlinedIcon />
             </div>
         </div>
     )
